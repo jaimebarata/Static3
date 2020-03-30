672337c01e10b155327bb0be8a365da07385e6d0
BuildTools <unconfigured@null.spigotmc.org>
2020-03-30 16:14:32 +0100

    Add project files.

---
+++ Static3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Static3
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string temp = "";
+			Console.WriteLine("Escreva a temperatura e a escala(Celsius/Fahrenheit): ");
+			temp = Console.ReadLine();
+		}
+	}
+}
